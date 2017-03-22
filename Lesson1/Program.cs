79b4669da0e4f31ab335425f4543bc7f4c9679e6
Vovanok <vovanok.net@gmail.com>
2017-03-22 23:59:00 +0300

    Разбор ДЗ 1

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,15 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lesson1
 {
     class Program
     {
+        static bool TryEnterNumberFromConsole(out uint number)
+        {
+            Console.Write("Введите число: ");
+            string numberAsString = Console.ReadLine();
+
+            if (!(uint.TryParse(numberAsString, out number)))
+            {
+                Console.Write("Ввели не число");
+                return false;
+            }
+
+            return true;
+        }
+
+        static void PrintFibNumberLessThanNumber(uint number)
+        {
+            Console.Write("Числа Фибоначчи меньшие числа " + number.ToString() + ": ");
+            uint firstFibNumber = 0;
+            uint secondFibNumber = 1;
+            if (number != 0)
+            {
+                Console.Write(firstFibNumber + " ");
+            }
+            
+            while (secondFibNumber < number)
+            {
+                Console.Write(secondFibNumber + " ");
+                uint nextSecondNumber = firstFibNumber + secondFibNumber;
+                firstFibNumber = secondFibNumber;
+                secondFibNumber = nextSecondNumber;
+            }
+
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
+            uint number1;
+            uint number2;
+            uint number3;
+
+            if (!TryEnterNumberFromConsole(out number1) ||
+                !TryEnterNumberFromConsole(out number2) ||
+                !TryEnterNumberFromConsole(out number3))
+            {
+                Console.Read();
+                return;
+            }
+
+            uint minNumber = Math.Min(Math.Min(number1, number2), number3);
+            Console.Write("Минимальное число: ");
+            Console.WriteLine(minNumber);
+
+            PrintFibNumberLessThanNumber(number1);
+            PrintFibNumberLessThanNumber(number2);
+            PrintFibNumberLessThanNumber(number3);
+
+            Console.Read();
         }
     }
 }
