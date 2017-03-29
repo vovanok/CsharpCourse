cf1f5a03566e38cdb5e6a666af78d05b9bd874cf
Vovanok <vovanok.net@gmail.com>
2017-03-29 22:14:20 +0300

    Разбор ДЗ 3

---
+++ Lesson3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string enteredString = Console.ReadLine();
+
+            StringHelper sHelper = new StringHelper(enteredString);
+
+            string isPolyndromAnswer = sHelper.IsChangeling() ? "Является" : "Не является";
+            Console.WriteLine($"{isPolyndromAnswer} полиндромом");
+            Console.WriteLine($"Количество слов: {sHelper.GetCountWords()}");
+            Console.WriteLine($"Перевернутая строка: {sHelper.GetInverseString()}");
+
+            Console.Read();
+        }
+    }
+}
---
+++ Lesson3/StringHelper.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson3
+{
+    class StringHelper
+    {
+        private string inputString;
+
+        public StringHelper(string inputString)
+        {
+            this.inputString = inputString;
+        }
+
+        public bool IsChangeling()
+        {
+            if (inputString == null)
+                return false;
+
+            return inputString == GetInverseString();
+        }
+
+        public uint GetCountWords()
+        {
+            if (inputString == null)
+                return 0;
+
+            List<char> separators = new List<char>();
+            foreach (char ch in inputString)
+            {
+                if (!char.IsLetterOrDigit(ch) && !separators.Contains(ch))
+                    separators.Add(ch);
+            }
+
+            string[] words = inputString.Split(separators.ToArray());
+
+            uint countWords = 0;
+            foreach (string word in words)
+            {
+                if (!string.IsNullOrEmpty(word))
+                {
+                    countWords++;
+                }
+            }
+
+            return countWords;
+        }
+
+        public string GetInverseString()
+        {
+            if (inputString == null)
+                return string.Empty;
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i = inputString.Length - 1; i >= 0; i--)
+            {
+                result.Append(inputString[i]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
